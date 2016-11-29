7a0bef209be3ee95d29435cd3ee476ed64bb452d
Usman Qadri <usmanqadri@Usmans-MacBook-Pro.local>
2016-11-29 13:59:55 +0500

    Initial Commit for Barcode Scanner

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # CordovaBarCodeScanner
 Sample Project for Barcode Scanning
+
+1- Clone the Repoitory
+2- Use the command to restore application's state:
+```
+ionic state restore 
+```
+3- Build the application
+```
+ionic build [ios/android] 
+```
+4- Click the button to scan the barcode
---
+++ barcodeScanner/www/js/app.js
@@ -0,0 +1,46 @@
+// Ionic Starter App
+
+// angular.module is a global place for creating, registering and retrieving Angular modules
+// 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
+// the 2nd parameter is an array of 'requires'
+angular.module('starter', ['ionic'])
+
+.run(function($ionicPlatform,$rootScope) {
+  $ionicPlatform.ready(function() {
+    if(window.cordova && window.cordova.plugins.Keyboard) {
+      // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
+      // for form inputs)
+      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+
+      // Don't remove this line unless you know what you are doing. It stops the viewport
+      // from snapping when text inputs are focused. Ionic handles this internally for
+      // a much nicer keyboard experience.
+      cordova.plugins.Keyboard.disableScroll(true);
+    }
+    if(window.StatusBar) {
+      StatusBar.styleDefault();
+    }
+
+    $rootScope.scan = function(){
+      cordova.plugins.barcodeScanner.scan(
+        function (result) {
+          alert("Barcode Data:\n" +
+            "Result: " + result.text + "\n" +
+            "Format: " + result.format + "\n" +
+            "Cancelled: " + result.cancelled);
+        },
+        function (error) {
+          alert("Scanning failed: " + error);
+        },
+        {
+          "preferFrontCamera" : false, // iOS and Android
+          "showFlipCameraButton" : true, // iOS and Android
+          "prompt" : "Place a barcode inside the scan area", // supported on Android only
+          "formats" : "QR_CODE,PDF_417", // default: all but PDF_417 and RSS_EXPANDED
+          "orientation" : "landscape" // Android only (portrait|landscape), default unset so it rotates with the device
+        }
+      );
+    }
+
+  });
+});
